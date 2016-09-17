e6d3da308181d0ae67e6867c87a0176e10f40f5f
Amelia Brown <amelia.brown389@gmail.com>
2016-09-16 18:15:55 -0700

    (fix): add newer version of createRoom.jsx

--- public/views/createRoom.jsx
+++ public/views/createRoom.jsx
@@ -3,93 +3,86 @@ import React from 'react'
 
 //Simple ready view
 export default class CreateRoom extends React.Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
     this.state = {
       rooms: []
     };
-  }
-  componentWillMount() {
-    var currentRoom;
-    var self = this;
-    socket.on('enterRoom', function(roomName, canvas) {
-      window.roomName = roomName;
-      window.canvas = canvas;
-      window.host = true;
-      window.location.href = '#/drawing';
-      socket.removeListener('allRooms');
-    });
+	}
 
+	componentWillMount() {
+		var idString = '/#' + socket.id;
+		socket.on('enterRoom', function(roomName, roomObj) {
+			window.roomName = roomName;
+  		window.canvas = roomObj.canvas;
+  		window.username = roomObj.users;
+			window.host = true;
+			window.location.href = '#/drawing';
+  		socket.removeListener('allRooms');
+		});
+
+
+		socket.on('joined', function(didJoin, roomName, roomObj) {
+			if (didJoin) {
+				window.roomName = roomName;
+				window.host = false;
+				window.canvas = roomObj.canvas;
+				window.username = roomObj.users;
+				window.location.href = '#/drawing';
+			} else {
+				console.log('That room does not exist');
+			}
+		});
+	}
+
+	componentDidMount() {
+		var self = this;
     socket.emit('getRooms');
 
     socket.on('allRooms', function(rooms) {
-      console.log('all rooms: ', rooms);
-      this.setState({rooms: rooms});
-    }.bind(this));
-
-    socket.on('joined', function(didJoin, roomName, canvas) {
-      if (didJoin) {
-        window.roomName = roomName;
-        window.host = false;
-        window.canvas = canvas;
-        window.location.href = '#/drawing';
-      } else {
-        console.log('That room does not exist');
-      }
+    	console.log('all rooms: ', rooms);
+      self.setState({rooms: rooms});
+    });
+    $('#submitted').on('click',function(){
+      // console.log('Submitted');
+      setInterval(function(){
+        $('#createPageButton').click()
+      },2000);
     })
-  }
-
-<<<<<<< HEAD
-	componentDidMount(){
-		var that = this;
-		console.log('I mounted');
-		console.log()
-		$('#submitted').on('click',function(){
-			console.log('Submitted');
-			setInterval(function(){
-				$('#createPageButton').click()
-			},2000);
-		})
-
 	}
 
-	startSession(title) {
-		socket.emit('createRoom', {host:title});
+	startSession(title, username) {
+		socket.emit('createRoom', title, username);
 		// document.getElementById('roomTitle').value = '';
 	}
 
-	joinRoom(roomName) {
-		socket.emit('joinRoom', roomName);
+	joinRoom(roomName, username) {
+		socket.emit('joinRoom', roomName, username);
 		// document.getElementById('roomTitle').value = '';
 	}
 
 	render() {
+  //  	<input type="text" id="hostTitle" name="roomtitle" placeholder="Title here..." />
+		// <button className="btn waves-effect waves-light">
+		// 	Create a room
+		// </button>
 		return (
 		<div className="readyScreen valign">
-			<h3 className="tlt"> Join Room Session </h3>
->>>>>>> fc30aeff2a18e47c87b3e24ad553766685a43080
+		<input type="text" id="username" placeholder="Username here..." />
+			<h3 className="tlt"> Join Rooom Session </h3>
       <ul> {this.state.rooms.map(function(room, index) {
         return(
-          <li key={index} onClick={() => {this.joinRoom(room)}}>{room}</li>
+          <li key={index} onClick={() => {var username = document.getElementById('username').value; this.joinRoom(room, username)}}>{room}</li>
         )
       }.bind(this))}
-<<<<<<< HEAD
       </ul>
-			<h3 className="tlt"> Create Room Session </h3>
+			<h3 className="tlt"> Create Rooom Session </h3>
 			<form id="filesender" action = "file_upload" encType="multipart/form-data" method = "Post">
-				 <input type="text" id="hostTitle" name="roomtitle" placeholder="Title here..." />
-				 <input type="file" name="video" ></input>
-					<button type= "submit" id="submitted" className="btn waves-effect waves-light">Create a room with this video</button>
-			</form>
-			<script>console.log('HEY');$('#fname').val('Hey')</script>
-			<button hidden
-				id="createPageButton"
-				className="btn waves-effect waves-light"
-				onClick={() => {var title = document.getElementById('hostTitle').value;
-
-				this.startSession(title)}}>
-				Create a room
-			</button>
+			<input type="text" name="roomtitle" id="hostTitle" placeholder="Title here..." />
+         <input type="file" name="video" ></input>
+            <button type= "submit" id="submitted" className="btn waves-effect waves-light" >Create a room with this video</button>
+      </form>
+      <button hidden id="createPageButton" onClick={() => {var title = document.getElementById('hostTitle').value; var username = document.getElementById('username').value; this.startSession(title, username)}}></button>
 		</div>
 	)}
 }
